7316c874aac3e0f41edb81026cef3c868071c833
Eliud2005 <utti232008@utvco.edu.mx>
2025-06-26 23:01:05 -0600

    Mas cambios

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,23 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/jwt.constant';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rol } from 'src/rol/entities/rol.entity';
+import { RolService } from 'src/rol/rol.service';
+import { RolModule } from 'src/rol/rol.module';
 
 @Module({
-     imports:[UsersModule,
-      JwtModule.register({
+  imports: [
+    UsersModule,
+    RolModule,
+    TypeOrmModule.forFeature([Rol]), // ✅ Esto va fuera del JwtModule
+    JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '1d' },
     }),
-     ],
-    controllers:[AuthController],
-    providers:[AuthService]
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
-export class AuthModule {
-   
-}
+export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,26 +4,37 @@ import { RegisterDto } from './dto/register.dto';
 import * as bcryptjs from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rol } from 'src/rol/entities/rol.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+      @InjectRepository(Rol)
+  private readonly rolRepository: Repository<Rol>,
   ) {}
 
   async register({ username, email, password, role_id }: RegisterDto) {
-    const userByEmail = await this.usersService.findOneByName(email);
-    if (userByEmail) {
-      throw new BadRequestException('Ese correo ya existe');
-    }
-    return await this.usersService.create({
-      username,
-      email,
-      password: await bcryptjs.hash(password, 10),
-      role_id,
-    });
+  const userByEmail = await this.usersService.findOneByName(email);
+  if (userByEmail) {
+    throw new BadRequestException('Ese correo ya existe');
   }
+
+  const role = await this.rolRepository.findOneBy({ id: role_id });
+  if (!role) {
+    throw new BadRequestException(`No existe un rol con ID ${role_id}`);
+  }
+
+  return await this.usersService.create({
+    username,
+    email,
+    password: await bcryptjs.hash(password, 10),
+    role_id,
+  });
+}
 
   async login({ email, password }: LoginDto) {
     const user = await this.usersService.findOneByName(email);
