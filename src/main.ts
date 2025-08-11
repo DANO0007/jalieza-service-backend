b23a0d927ced15f50479606bb4688a354ed2704f
Eliud2005 <utti232008@utvco.edu.mx>
2025-08-11 01:06:44 -0600

    Variable .env para front

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use(cookieParser()); // ✅ Habilita cookies
 
   app.enableCors({
-    origin: 'http://localhost:8100', // ⚠️ Cambia esto si usas otro frontend
+    origin: process.env.FRONTEND_URL || 'http://localhost:8100', // ⚠️ Cambia esto si usas otro frontend
     credentials: true, // ✅ Permite enviar cookies entre frontend y backend
   });
 
